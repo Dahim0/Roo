1997464aab5ead2560e7ffdebe9ad38ab688c80f
Dahim0 <147000459+Dahim0@users.noreply.github.com>
2023-10-04 17:41:32 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,12 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>Welcome to robbyt website</H1>
+    <p>hello world</p>
+</body>
+</html>
